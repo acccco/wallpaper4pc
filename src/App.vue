c56ec1e30f514a170b1a211c956bfc3780404715
aco <wwsxuan@163.com>
2019-06-05 16:19:09 +0800

    构思

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,48 @@
 <template>
   <div id="app">
-    <nav class="nav">
-    </nav>
-    <router-view/>
+    <header class="x-row">
+      <DatePicker
+        align="center"
+        type="date"
+        placeholder="选择日期"
+        v-model="dataValue"
+        :picker-options="pickerOptions"
+      ></DatePicker>
+    </header>
+    <div class="x-row wallpaper-container"></div>
+    <footer class="x-row">
+      <Pagination
+        background
+        layout="prev, pager, next, jumper"
+        :current-page="pageNow"
+        :page-sizes="[15, 30, 45, 60]"
+        :page-size="15"
+        :total="400"
+      ></Pagination>
+    </footer>
   </div>
 </template>
 
 <script>
+  import {DatePicker, Pagination} from 'element-ui';
 
   export default {
     name: 'app',
-    components: {}
+    components: {
+      DatePicker,
+      Pagination
+    },
+    data() {
+      return {
+        dataValue: '',
+        pickerOptions: {
+          disabledDate(time) {
+            return time.getTime() > Date.now();
+          }
+        },
+        pageNow: 1
+      };
+    },
+    methods: {}
   };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import './scss/index.scss';
+import 'element-ui/lib/theme-chalk/index.css';
 
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router';
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app');
