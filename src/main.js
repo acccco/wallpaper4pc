c13fdbdc3139bacc378f2bc7ee30038e1e43ffde
aco <wwsxuan@163.com>
2019-06-05 14:44:44 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import './scss/index.scss';
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount('#app');
