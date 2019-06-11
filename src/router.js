1b3d3e96de20a0216a3aac4b17697d0200d8e4b6
aco <wwsxuan@163.com>
2019-06-11 13:41:43 +0800

    单图片页面

---
+++ src/router.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import OnePicture from './views/OnePicture.vue';
+import List from './views/List.vue';
+import About from './views/About.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'onePicture',
+      component: OnePicture
+    },
+    {
+      path: '/today',
+      redirect: '/'
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: List
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
+});
---
+++ src/views/OnePicture.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="one-picture">
+    <div class="prev btn"></div>
+    <div class="next btn"></div>
+    <Matrix ref="matrix"></Matrix>
+  </div>
+</template>
+
+<script>
+  import Matrix from '@/components/Matrix';
+
+  export default {
+    name: 'onePicture',
+    components: {
+      Matrix
+    }
+  };
+</script>
